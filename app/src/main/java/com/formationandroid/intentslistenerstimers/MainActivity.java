9690a2b91dc51210d3c06d1b2d468fa4bd1b6327
Matthieu Regnauld <mregnauld@gmail.com>
2018-01-08 22:21:56 +0100

    Contenu de l'exercice

---
+++ app/src/main/java/com/formationandroid/intentslistenerstimers/MainActivity.java
@@ -0,0 +1,111 @@
+package com.formationandroid.intentslistenerstimers;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+	
+	// Constantes :
+	private static final int ID_RETOUR_SAISIE = 123;
+	
+	// Vues :
+	private TextView textViewDecompte = null;
+	private Button buttonEnvoyer = null;
+	
+	// Timer :
+	private CountDownTimer countDownTimer = null;
+	
+	// Décompte :
+	private int decompte = 0;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		// init :
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		// vues :
+		textViewDecompte = findViewById(R.id.texte_decompte);
+		buttonEnvoyer = findViewById(R.id.bouton_envoyer);
+		
+		// listeners :
+		textViewDecompte.setOnClickListener(this);
+		buttonEnvoyer.setOnClickListener(this);
+	}
+	
+	@Override
+	public void onClick(View view)
+	{
+		if (view == textViewDecompte)
+		{
+			// ouverture d'une nouvelle activité avec attente de résultat :
+			Intent intent = new Intent(this, SaisieActivity.class);
+			startActivityForResult(intent, ID_RETOUR_SAISIE);
+		}
+		else if (view == buttonEnvoyer)
+		{
+			// préparation de l'envoi d'un message :
+			Intent intent = new Intent(Intent.ACTION_SEND);
+			intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.main_message_decompte, decompte));
+			intent.setType("text/plain");
+			
+			// vérification et envoi :
+			if (intent.resolveActivity(getPackageManager()) != null)
+			{
+				startActivity(intent);
+			}
+		}
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		// init :
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		// récupération du résultat de l'activité de saisie :
+		if (requestCode == ID_RETOUR_SAISIE && resultCode == Activity.RESULT_OK)
+		{
+			// valeur du décompte, en secondes :
+			decompte = data.getIntExtra(SaisieActivity.EXTRA_DECOMPTE, 0);
+			
+			// si timer en cours, on l'arrête :
+			if (countDownTimer != null)
+			{
+				countDownTimer.cancel();
+				countDownTimer = null;
+			}
+			
+			// lancement du timer :
+			countDownTimer = new CountDownTimer(decompte * 1000, 100)
+			{
+				@Override
+				public void onTick(long millisUntilFinished)
+				{
+					String libelleDecompte = ((int) Math.floor(millisUntilFinished / 1000) + 1) + " s";
+					textViewDecompte.setText(libelleDecompte);
+				}
+				
+				@Override
+				public void onFinish()
+				{
+					textViewDecompte.setText(R.string.main_libelle_creer_decompte);
+				}
+			};
+			countDownTimer.start();
+			
+			// affichage du bouton envoyer :
+			buttonEnvoyer.setVisibility(View.VISIBLE);
+		}
+	}
+	
+}
---
+++ app/src/main/java/com/formationandroid/intentslistenerstimers/SaisieActivity.java
@@ -0,0 +1,58 @@
+package com.formationandroid.intentslistenerstimers;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SaisieActivity extends AppCompatActivity
+{
+	
+	// Constantes :
+	public static final String EXTRA_DECOMPTE = "EXTRA_DECOMPTE";
+	
+	// Vues :
+	private EditText editTextDecompte = null;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		// init :
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_saisie);
+		
+		// vues :
+		editTextDecompte = findViewById(R.id.saisie_decompte);
+	}
+	
+	/**
+	 * Listener clic bouton valider.
+	 * @param view Bouton valider
+	 */
+	public void onClickBoutonValider(View view)
+	{
+		// valeur du décompte :
+		try
+		{
+			int decompte = Integer.parseInt(editTextDecompte.getText().toString());
+			
+			// renvoi du décompte à l'activité appelante :
+			Intent intent = new Intent();
+			intent.putExtra(EXTRA_DECOMPTE, decompte);
+			setResult(Activity.RESULT_OK, intent);
+			
+			// fermeture de cette activité :
+			finish();
+		}
+		catch (Exception e)
+		{
+			// message d'erreur :
+			Toast.makeText(this, R.string.saisie_message_erreur, Toast.LENGTH_LONG).show();
+		}
+	}
+	
+}
