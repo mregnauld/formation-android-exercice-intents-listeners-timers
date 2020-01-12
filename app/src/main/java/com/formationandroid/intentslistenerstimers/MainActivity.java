193411ed4810b86d6a39f6cbd14b6aba566e002a
mregnauld <mregnauld@gmail.com>
2020-01-12 10:50:08 +0100

    Mise a jour Android 10

--- app/src/main/java/com/formationandroid/intentslistenerstimers/MainActivity.java
+++ app/src/main/java/com/formationandroid/intentslistenerstimers/MainActivity.java
@@ -4,10 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
